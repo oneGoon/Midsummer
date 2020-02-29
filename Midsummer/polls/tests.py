02ad610dc9e54627de0b534942aaeb550225453e
Alan.Wang1 <alan.wang1@homecredit.cn>
2020-02-29 23:53:18 +0800

    init project

---
+++ Midsummer/polls/tests.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+
+import datetime
+
+from django.utils import timezone
+from .models import Question
+from django.urls import reverse
+
+def create_question(question_text, days):
+	time = timezone.now() + datetime.timedelta(days=days)
+	return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionModelTests(TestCase):
+	def test_no_questions(self):
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+	def test_past_question(self):
+		create_question(question_text="Past question.", days=-30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(
+			response.context['latest_question_list'],
+			['<Question: Past question.>']
+			)
+
+
+
+	"""docstring for QuestionModelTests"""
+	def test_was_published_recently_with_future_question(self):
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		self.assertIs(future_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_old_question(self):
+		time = timezone.now() - datetime.timedelta(days=30)
+		old_question = Question(pub_date=time)
+		self.assertIs(old_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_recently_question(self):
+		time = timezone.now() - datetime.timedelta(hours=23,minutes=59)
+		recently_question = Question(pub_date=time)
+		self.assertIs(recently_question.was_published_recently(), True)
